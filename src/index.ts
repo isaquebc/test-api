f1207911a0045d37c4b9783d81bfb393bd91e6b9
Isaque Costa <isaquebc@gmail.com>
2024-12-22 12:50:22 -0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import express, { Request, Response } from 'express';
 import Mall from 'open-marketplace-getters';
+import axios from 'axios';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', async (req: Request, res: Response) => {
-  console.log('Hello')
   // Assuming the correct method is 'getProductsBySeller'
   const store = new Mall.MercadoLivreStore();
 
-  const data = await store.getSellerProducts('298668904');
+  const data = await store.searchInSeller('298668904', 'fifa');
+  const data1 = await store.getSellerProducts('298668904');
+
   res.send(data);
+});
+app.get('/1', async (req: Request, res: Response) => {
+  // Assuming the correct method is 'getProductsBySeller'
+  const store = new Mall.MercadoLivreStore();
+  const data = await store.searchInSeller('298668904', 'fifa');
+  const data1 = await store.getSellerProducts('298668904') as any;
+  const resp = await store.getProductLink(data1.results[0].id);
+  res.send(resp);
 });
 
 app.listen(port, () => {
